9f13b293f006a8bc6e9cf60d578cc9eaaffcd820
thiago-tanaka <thiago.tanaka.dev@gmail.com>
2025-04-12 16:26:34 +0900

    adjust typing speed and accuracy thresholds

--- app/Models/Digitacao.php
+++ app/Models/Digitacao.php
@@ -7,12 +7,12 @@ namespace App\Models;
 class Digitacao
 {
     public const VELOCIDADE_OTIMA = 250;
-    public const VELOCIDADE_BOA = 200;
-    public const VELOCIDADE_MEDIA = 140;
+    public const VELOCIDADE_BOA = 160;
+    public const VELOCIDADE_MEDIA = 100;
 
     public const PRECISAO_OTIMA = 98;
-    public const PRECISAO_BOA = 95;
-    public const PRECISAO_MEDIA = 90;
+    public const PRECISAO_BOA = 97;
+    public const PRECISAO_MEDIA = 96;
 
     public const COR_RUIM = '#fc0434';
     public const COR_MEDIA = '#ffaa00';
